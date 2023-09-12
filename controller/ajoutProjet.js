ca7fbb1bc79b6ef3d5ac23b3adbfc44108831d86
kouame konan abraham <abrahamkonany@gmail.com>
2023-09-12 08:42:08 +0000

    ok

--- controller/ajoutProjet.js
+++ controller/ajoutProjet.js
@@ -4,7 +4,15 @@ class AjoutProjet{
     static async createProjet(req, res){
         try {
             const {image, ...body} = req.body;
-            
+            await ajoutProjet.create({
+                image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+                ...body
+            })
+            res.status(200).json({
+                status:true,
+                message: "Projet encours de validation !!"
+            })
+
         } catch (e) {
             if( e instanceof MongooseError) throw new Error("Erreur de server Mongose:",e.message)
             res
